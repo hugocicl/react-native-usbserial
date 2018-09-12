9f34b1c67b7b6a47bd055e15a3095578ee202924
Hugo Brito <hugobrito@MacBook-Pro-de-apple.local>
2018-09-12 14:40:58 +0100

    change Android API 25 and comunication 9600

--- android/src/main/java/com/bmateam/reactnativeusbserial/ReactUsbSerialModule.java
+++ android/src/main/java/com/bmateam/reactnativeusbserial/ReactUsbSerialModule.java
@@ -115,7 +115,7 @@ public class ReactUsbSerialModule extends ReactContextBaseJavaModule {
         UsbSerialPort port = driver.getPorts().get(0);
 
         port.open(connection);
-        port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+        port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
 
         String id = generateId();
         UsbSerialDevice usd = new UsbSerialDevice(port);
